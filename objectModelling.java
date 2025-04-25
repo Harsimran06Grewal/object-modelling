ae0c0139849529e970b90cdb8136ab800cba4ae5
Harsimran Kaur <harsimran0596.be21@chitkara.edu.in>
2025-04-25 09:39:25 +0530

    Harsimran add : Solving Bag and Ball Problem using Object Modelling

--- objectModelling.java
+++ objectModelling.java
@@ -30,37 +30,31 @@ class Bag{
         }
     
         // true if red balls are less than green balls
-        if (ball.color == Color.RED) {
-            if (rCount + 1 < gCount) {
-                rCount++;
-            } 
-            else {
-                return false;
-            }
+        if (ball.color == Color.RED && rCount + 1 > gCount) {
+            return false;
         } 
         // true if yellow balls are less than or equal or 40 percent of bag capacity
-        else if (ball.color == Color.YELLOW) {
-            if (yCount + 1 <= (0.4 * capacity)) {
-                yCount++;
-            } 
-            else {
-                return false;
-            }
-        } 
-        else if (ball.color == Color.GREEN) {
-            gCount++;
-        } 
-        else {
-            return false; 
+        else if (ball.color == Color.YELLOW && yCount + 1 >= (0.4 * capacity)) {
+            return false;
         }
-    
+
         // if above conditions are true then balls are added to list
         balls.add(ball);
         totalCount++;
+
+        if(ball.color.equals(Color.RED)){
+            rCount++;
+        }
+        else if(ball.color.equals(Color.YELLOW)){
+            yCount++;
+        }
+        else if(ball.color.equals(Color.GREEN)){
+            gCount++;
+        }
+
         return true;
     }    
 }
-
 public class ObjectModelling {
     public static void main(String args[]){
         Bag bag = new Bag();
